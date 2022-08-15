66b33637091125dc803019c99995eb7c3680996e
John Maximilian <2e0byo@gmail.com>
2022-08-15 18:39:29 +0200

    test: fix schema test for new schema.

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -18,6 +18,6 @@ def test_get_config_schema():
     schema = ext.get_config_schema()
 
     # Test the content of your config schema
-    assert 'username' in schema
-    assert 'password' in schema
     assert "quality" in schema
+    assert "client_id" in schema
+    assert "client_secret"
