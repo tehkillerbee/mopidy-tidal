4a7ab0bbbb7aad86d85b31b2947898dbffe5391c
John Maximilian <2e0byo@gmail.com>
2022-08-15 18:49:17 +0200

    test: check setup method registers right class.

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from mopidy_tidal import Extension
+from mopidy_tidal.backend import TidalBackend
 
 
 def test_get_default_config():
@@ -21,3 +22,13 @@ def test_get_config_schema():
     assert "quality" in schema
     assert "client_id" in schema
     assert "client_secret"
+
+
+def test_setup(mocker):
+    ext = Extension()
+    registry = mocker.Mock()
+    ext.setup(registry)
+    registry.add.assert_called_once()
+    args = registry.add.mock_calls[0].args
+    assert args[0] == "backend"
+    assert type(args[1]) is type(TidalBackend)
