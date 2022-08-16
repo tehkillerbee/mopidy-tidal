660ceaf66d0c766105c1f8ccdda61dc3addcdf13
John Maximilian <2e0byo@gmail.com>
2022-08-16 13:34:58 +0200

    test: playback.

---
+++ tests/test_playback.py
@@ -0,0 +1,28 @@
+from mopidy_tidal.playback import TidalPlaybackProvider
+
+
+def test_playback_old_api(mocker):
+    uniq = object()
+    get_media_url = mocker.Mock()
+    get_media_url.return_value = uniq
+    session = mocker.Mock(get_media_url=get_media_url)
+    backend = mocker.Mock(_session=session)
+    audio = mocker.Mock()
+    tpp = TidalPlaybackProvider(audio, backend)
+    assert tpp.translate_uri("tidal:track:1:2:3") is uniq
+    session.get_media_url.assert_called_once_with(3)
+
+
+def test_playback_new_api(mocker):
+    uniq = object()
+    session = mocker.Mock(spec=["track"])
+    session.mock_add_spec(["track"])
+    track = mocker.Mock()
+    track.get_url.return_value = uniq
+    session.track.return_value = track
+    backend = mocker.Mock(_session=session)
+    audio = mocker.Mock()
+    tpp = TidalPlaybackProvider(audio, backend)
+    assert tpp.translate_uri("tidal:track:1:2:3") is uniq
+    session.track.assert_called_once_with(3)
+    track.get_url.assert_called_once()
