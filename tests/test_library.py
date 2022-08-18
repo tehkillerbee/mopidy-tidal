a84016612544900b30cf784133597f9a6f395700
John Maximilian <2e0byo@gmail.com>
2022-08-18 15:15:30 +0200

    test(library): image caching.

--- tests/test_library.py
+++ tests/test_library.py
@@ -36,6 +36,27 @@ def test_get_album_image_new_api(images_getter, mocker, dimensions):
     assert get_uri_args == [dimensions]
 
 
+def test_get_album_no_image_new_api(images_getter, mocker):
+    ig, session = images_getter
+    uri = "tidal:album:1-1-1"
+    get_album = mocker.Mock()
+
+    def get_uri(*_):
+        raise ValueError()
+
+    get_album.image = get_uri
+    session.get_album.return_value = get_album
+    assert ig(uri) == (uri, [])
+
+
+def test_get_album_no_getter_methods_new_api(images_getter, mocker):
+    ig, session = images_getter
+    uri = "tidal:album:1-1-1"
+    get_album = mocker.Mock(spec={"id", "__name__"}, name="get_album", id="1")
+    session.get_album.return_value = get_album
+    assert ig(uri) == (uri, [])
+
+
 @pytest.mark.parametrize("dimensions", (750, 640, 480))
 def test_get_album_image_old_api(images_getter, mocker, dimensions):
     ig, session = images_getter
