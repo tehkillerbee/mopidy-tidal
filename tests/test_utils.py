37e01c657f4dd69e14c1cacee587924937ecac50
John Maximilian <2e0byo@gmail.com>
2022-08-24 15:44:40 +0200

    test: utils.
    I don't test get_query_params as nothing appears to use it.

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from mopidy_tidal.utils import apply_watermark, remove_watermark
+
+
+def test_apply_watermark():
+    assert apply_watermark("track") == "track [TIDAL]"
+
+
+def test_remove_watermark():
+    assert remove_watermark("track [TIDAL]") == "track"
