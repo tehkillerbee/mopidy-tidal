c4c05c9b053cdb81044960e16e5dbac0cf8c4637
John Maximilian <2e0byo@gmail.com>
2022-08-27 16:07:28 +0200

    fix(setup.py): version function.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 def get_version(filename):
     with open(filename) as fh:
-        metadata = dict(re.findall('__([a-z]+)__ = "([^\']+)"', fh.read()))
+        metadata = dict(re.findall('__([a-z]+)__ = "([^"]+)"', fh.read()))
         return metadata["version"]
 
 
