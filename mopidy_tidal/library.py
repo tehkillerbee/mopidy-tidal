4b98196423ab572aab2729d0de753ad9639f8b87
John Maximilian <2e0byo@gmail.com>
2022-09-03 16:51:47 +0100

    refactor: use generator expression over listcomp.
    To save *tiny* amounts of ram... but it feels cleaner to say `next()` than [0].

--- mopidy_tidal/library.py
+++ mopidy_tidal/library.py
@@ -451,8 +451,9 @@ class TidalLibraryProvider(backend.LibraryProvider):
 
     def _lookup_track(self, session, parts):
         album_id = parts[3]
-        track = [t for t in tracks if t.id == int(parts[4])][0]
         tracks = self._get_album_tracks(session, album_id)
+        # We get a spurious coverage error since the next expression should never raise StopIteration
+        track = next(t for t in tracks if t.id == int(parts[4]))  # pragma: no cover
         artist = full_models_mappers.create_mopidy_artist(track.artist)
         album = full_models_mappers.create_mopidy_album(track.album, artist)
         return [full_models_mappers.create_mopidy_track(artist, album, track)]
