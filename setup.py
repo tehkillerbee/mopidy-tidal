6efe96ecb51992c5274d61cae5cae41b50767693
Fabio Manganiello <fabio@manganiello.tech>
2023-03-15 13:18:48 +0100

    Temporary fix for the missing user picture error on tidalapi

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "setuptools",
         "Mopidy >= 3.0",
         "Pykka >= 1.1",
-        "tidalapi >= 0.7.0",
+        "tidalapi @ git+https://github.com/BlackLight/python-tidal",
         "requests >= 2.0.0",
     ],
     entry_points={
