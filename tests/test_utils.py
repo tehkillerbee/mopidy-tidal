e5addc69b4a68bf7305172f4a8e68cf43a2113a8
John Maximilian <2e0byo@gmail.com>
2024-02-03 15:10:52 +0000

    test: watermarks.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
 from mopidy_tidal.utils import apply_watermark, remove_watermark
 
 
-def test_apply_watermark():
+def test_apply_watermark_adds_tidal():
     assert apply_watermark("track") == "track [TIDAL]"
 
 
-def test_remove_watermark():
+def test_remove_watermark_removes_tidal_if_present():
     assert remove_watermark(None) is None
     assert remove_watermark("track [TIDAL]") == "track"
