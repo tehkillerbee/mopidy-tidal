408a0611ac45610342ddb79a1391677b58f0e0df
John Maximilian <2e0byo@gmail.com>
2024-02-03 15:35:13 +0000

    test: search cache.

--- tests/test_search_cache.py
+++ tests/test_search_cache.py
@@ -1,28 +1,28 @@
-import pytest
-
 from mopidy_tidal.lru_cache import SearchCache, SearchKey
 
 
 def test_search_cache_returns_cached_value_if_present(mocker):
     search_function = mocker.Mock()
     cache = SearchCache(search_function)
-    d1 = {"exact": True, "query": {"artist": "TestArtist", "album": "TestAlbum"}}
-    d1_sk = SearchKey(**d1)
-    uniq = object()
-    cache[str(d1_sk)] = uniq
+    query = {"exact": True, "query": {"artist": "TestArtist", "album": "TestAlbum"}}
+    search_key = SearchKey(**query)
+    cache[str(search_key)] = mocker.sentinel.results
 
-    assert cache("arg", **d1) is uniq
+    results = cache("arg", **query)
+
+    assert results is mocker.sentinel.results
     search_function.assert_not_called()
 
 
-def test_search_cache_not_cached(mocker):
-    func_ret = object
-    func = mocker.Mock()
-    func.return_value = func_ret
-    cache = SearchCache(func)
+def test_search_defers_to_search_function_if_not_present_and_stores(mocker):
+    search_function = mocker.Mock(return_value=mocker.sentinel.results)
+    cache = SearchCache(search_function)
+    query = {"exact": True, "query": {"artist": "TestArtist", "album": "TestAlbum"}}
+    search_key = SearchKey(**query)
+    assert str(search_key) not in cache
 
-    d1 = {"exact": True, "query": {"artist": "TestArtist", "album": "TestAlbum"}}
-    d1_sk = SearchKey(**d1)
-    assert str(d1_sk) not in cache
-    assert cache("arg", **d1) is func_ret
-    func.assert_called_once_with("arg", **d1)
+    results = cache("arg", **query)
+
+    assert results is mocker.sentinel.results
+    assert str(search_key) in cache
+    search_function.assert_called_once_with("arg", **query)
