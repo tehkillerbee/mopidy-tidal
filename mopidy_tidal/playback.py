711e311ecb7f3ad8ae68e6fa6cd46749fdef80b7
tehkillerbee <tehkillerbee@users.noreply.github.com>
2024-02-07 21:45:30 +0100

    Playback: Use streams instead of direct URL.

--- mopidy_tidal/playback.py
+++ mopidy_tidal/playback.py
@@ -11,6 +11,7 @@ import logging
 
 from mopidy import backend
 from tidalapi import Quality
+from tidalapi.media import ManifestMimeType
 from pathlib import Path
 
 from . import Extension, context
@@ -35,15 +36,24 @@ class TidalPlaybackProvider(backend.PlaybackProvider):
                     "No HI_RES available for this track; Using playback quality: %s",
                     "LOSSLESS",
                 )
-        if session.is_pkce:
-            manifest = session.track(track_id).get_stream().get_stream_manifest()
+
+        stream = session.track(track_id).get_stream()
+        manifest = stream.get_stream_manifest()
+        logger.info("MimeType:{}".format(stream.manifest_mime_type))
+        logger.info(
+            "Starting playback of track:{}, (quality:{}, codec:{}, {}bit/{}Hz)".format(track_id,
+                                                                                      stream.audio_quality,
+                                                                                      manifest.get_codecs(),
+                                                                                      stream.bit_depth,
+                                                                                      stream.sample_rate))
+        if stream.manifest_mime_type == ManifestMimeType.MPD.value:
             hls = manifest.get_hls()
-            hls_path = Path(Extension.get_cache_dir(context.get_config()), "hls.m3u8")
-            with open(hls_path, "w") as my_file:
-                my_file.write(hls)
-            logger.info("Starting playback of {}, (codec:{}, sampling rate:{} Hz)".format(hls_path, manifest.get_codecs(), manifest.get_sampling_rate()))
-            return "file://{}".format(hls_path)
-        else:
-            newurl = session.track(track_id).get_url()
-            logger.info("transformed into %s", newurl)
-        return newurl
+            if hls:
+                hls_path = Path(Extension.get_cache_dir(context.get_config()), "hls.m3u8")
+                with open(hls_path, "w") as my_file:
+                    my_file.write(hls)
+                return "file://{}".format(hls_path)
+            else:
+                raise AttributeError("No HLS stream available")
+        elif stream.manifest_mime_type == ManifestMimeType.BTS.value:
+            return manifest.get_urls()
