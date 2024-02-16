88235b0451cede5285d86dd37d870f4ca582acb4
tehkillerbee <tehkillerbee@users.noreply.github.com>
2024-02-16 23:38:42 +0100

    Refactor/cleanup, move load/save session to tidalapi.

--- mopidy_tidal/backend.py
+++ mopidy_tidal/backend.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-import json
 import logging
 from concurrent.futures import Future
 from pathlib import Path
@@ -9,34 +8,43 @@ from typing import Optional
 from mopidy import backend
 from pykka import ThreadingActor
 from tidalapi import Config, Quality, Session
-from tidalapi.session import LinkLogin
 
 from mopidy_tidal import Extension, context, library, playback, playlists
 
 logger = logging.getLogger(__name__)
 
 
-def _connecting_log(msg: str, level="info"):
-    getattr(logger, level)("Connecting to TIDAL... " + msg)
-
-
 class TidalBackend(ThreadingActor, backend.Backend):
     def __init__(self, config, audio):
         super().__init__()
-        self._active_session: Optional[Session] = None
-        self._logged_in = False
+        # Mopidy cfg
         self._config = config
         context.set_config(self._config)
+        self._tidal_config = config[Extension.ext_name]
+
+        # Backend
         self.playback = playback.TidalPlaybackProvider(audio=audio, backend=self)
         self.library = library.TidalLibraryProvider(backend=self)
         self.playlists = playlists.TidalPlaylistsProvider(backend=self)
-        self.uri_schemes = ["tidal"]
+
+        # Session parameters
+        self._active_session: Optional[Session] = None
+        self._logged_in: bool = False
+        self.uri_schemes: [str] = ["tidal"]
         self._login_future: Optional[Future] = None
         self._login_url: Optional[str] = None
-        self.login_method = "BLOCK"
-        self.data_dir = Path(Extension.get_data_dir(self._config))
-        self.oauth_file = self.data_dir / "tidal-oauth.json"
-        self.lazy_connect = False
+        self.data_dir: Path = Path(Extension.get_data_dir(self._config))
+        self.session_file_path: Path = Path("")
+
+        # Config parameters
+        # Lazy: Connect lazily, i.e. login only when user starts browsing TIDAL directories
+        self.lazy_connect: bool = False
+        # Login Method:
+        #   BLOCK:  Immediately prompt user for login (This will block mopidy startup!)
+        #   HACK:   Display URI with login URL. When clicked QR code and TTS is generated
+        self.login_method: str = "BLOCK"
+        # pkce_enabled: If true, TIDAL session will use PKCE auth. Otherwise OAuth2
+        self.pkce_enabled: bool = False
 
     @property
     def session(self):
@@ -47,68 +55,81 @@ class TidalBackend(ThreadingActor, backend.Backend):
     @property
     def logged_in(self):
         if not self._logged_in:
-            self._load_oauth_session()
+            if self._active_session.load_session_from_file(self.session_file_path):
+                logger.info("Loaded tidal session from file %s", self.session_file_path)
+                self._logged_in = True
         return self._logged_in
 
-    def _save_oauth_session(self):
-        # create a new session
-        if self._active_session.check_login():
-            # store current OAuth session
-            data = {}
-            data["token_type"] = {"data": self._active_session.token_type}
-            data["session_id"] = {"data": self._active_session.session_id}
-            data["access_token"] = {"data": self._active_session.access_token}
-            data["refresh_token"] = {"data": self._active_session.refresh_token}
-            logger.debug("Saving OAuth session to %s" % self.oauth_file)
-            with self.oauth_file.open("w") as outfile:
-                json.dump(data, outfile)
-            self._logged_in = True
-            logger.info("TIDAL Login OK")
+    @property
+    def session_valid(self):
+        # Returns true when session is logged in and valid
+        return self._active_session.check_login()
 
     def on_start(self):
-        user_config = self._config["tidal"]
-        quality = user_config["quality"]
-        _connecting_log("Quality = %s" % quality)
+        quality = self._tidal_config["quality"]
         config = Config(quality=Quality(quality))
-        client_id = user_config["client_id"]
-        client_secret = user_config["client_secret"]
-        self.login_method = user_config["login_method"]
-        self.lazy_connect = user_config["lazy"]
-        if self.login_method == "HACK" and not user_config["lazy"]:
-            _connecting_log(
-                "HACK login implies lazy connection, setting lazy=True.",
-                level="warning",
-            )
+        client_id = self._tidal_config["client_id"]
+        client_secret = self._tidal_config["client_secret"]
+        self.login_method = self._tidal_config["login_method"]
+        self.lazy_connect = self._tidal_config["lazy"]
+        self.pkce_enabled = self._tidal_config["pkce_enabled"]
+        logger.info("Quality: %s", quality)
+        logger.info("Authentication: %s", "PKCE" if self.pkce_enabled else "OAuth")
+
+        # Set the session filename, depending on the type of session
+        if self.pkce_enabled:
+            self.session_file_path = Path(self.data_dir, "tidal-pkce.json")
+        else:
+            self.session_file_path = Path(self.data_dir, "tidal-oauth.json")
+
+        if self.login_method == "HACK" and not self._tidal_config["lazy"]:
+            logger.warning("HACK login implies lazy connection, setting lazy=True.")
             self.lazy_connect = True
-        _connecting_log(f"login method {self.login_method}.")
-        if (client_id and not client_secret) or (client_secret and not client_id):
-            _connecting_log(
-                "always provide client_id and client_secret together", "warning"
-            )
-            _connecting_log("using default client id & client secret from python-tidal")
+        logger.info("Login method %s", self.login_method)
 
         if client_id and client_secret:
-            _connecting_log("client id & client secret from config section are used")
+            logger.info("Using client id & client secret from config")
             config.client_id = client_id
             config.api_token = client_id
             config.client_secret = client_secret
-
-        if not client_id and not client_secret:
-            _connecting_log("using default client id & client secret from python-tidal")
+        elif (client_id and not client_secret) or (client_secret and not client_id):
+            logger.warning("Always provide both client_id and client_secret")
+            logger.info("Using default client id & client secret from python-tidal")
+        else:
+            logger.info("Using default client id & client secret from python-tidal")
 
         self._active_session = Session(config)
         if not self.lazy_connect:
             self._login()
 
     def _login(self):
-        self._load_oauth_session()
-        if not self._active_session.check_login():
-            logger.info("Creating new OAuth session...")
-            self._active_session.login_oauth_simple(function=logger.info)
-            self._save_oauth_session()
+        """Load session at startup or create a new session"""
+        if self._active_session.load_session_from_file(self.session_file_path):
+            logger.info(
+                "Loaded existing tidal session from file %s...", self.session_file_path
+            )
+        if not self.session_valid:
+            # Attempt to create new session
+            if self.pkce_enabled:
+                logger.info("Creating new session (PKCE)...")
+                self._active_session.login_oauth_simple(function=logger.info)
+                # self._active_session.save_session_to_file(self.session_file_path)
+            else:
+                logger.info("Creating new session (OAuth)...")
+                self._active_session.login_oauth_simple(function=logger.info)
+                # self._active_session.save_session_to_file(self.session_file_path)
 
-        if not self._active_session.check_login():
-            logger.info("TIDAL Login KO")
+        self._complete_login()
+
+    def _complete_login(self):
+        """Perform final steps of login sequence; save session to file"""
+        if self.session_valid:
+            # Check if session is valid after login
+            logger.info("TIDAL Login OK")
+            self._active_session.save_session_to_file(self.session_file_path)
+            self._logged_in = True
+        else:
+            logger.error("TIDAL Login Failed")
             raise ConnectionError("Failed to log in.")
 
     @property
@@ -118,31 +139,9 @@ class TidalBackend(ThreadingActor, backend.Backend):
 
     @property
     def login_url(self) -> Optional[str]:
+        """Start a new login sequence (if not active) and get the latest login URL"""
         if not self._logged_in and not self.logging_in:
             login_url, self._login_future = self._active_session.login_oauth()
-            self._login_future.add_done_callback(lambda *_: self._save_oauth_session())
-            # self._login_future.add_done_callback(lambda: self.logged_in=True)
+            self._login_future.add_done_callback(lambda *_: self._complete_login())
             self._login_url = login_url.verification_uri_complete
         return f"https://{self._login_url}" if self._login_url else None
-
-    def _load_oauth_session(self):
-        try:
-            oauth_file = self.oauth_file
-            with open(oauth_file) as f:
-                logger.info("Loading OAuth session from %s...", oauth_file)
-                data = json.load(f)
-
-            assert self._active_session, "No session loaded"
-            args = {
-                "token_type": data.get("token_type", {}).get("data"),
-                "access_token": data.get("access_token", {}).get("data"),
-                "refresh_token": data.get("refresh_token", {}).get("data"),
-            }
-
-            self._active_session.load_oauth_session(**args)
-        except Exception as e:
-            logger.info("Could not load OAuth session from %s: %s", oauth_file, e)
-
-        if self._active_session.check_login():
-            logger.info("TIDAL Login OK")
-            self._logged_in = True
