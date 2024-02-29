b24149c1deff7d0dad37801b39a75bd4e9a24e9c
tehkillerbee <tehkillerbee@users.noreply.github.com>
2024-02-29 23:42:08 +0100

    Cleanup unused param

--- mopidy_tidal/backend.py
+++ mopidy_tidal/backend.py
@@ -185,7 +185,6 @@ class TidalBackend(ThreadingActor, backend.Backend):
                 ] = self._active_session.pkce_get_auth_token(url_redirect)
                 # Parse and set tokens.
                 self._active_session.process_auth_token(json)
-                self.is_pkce = True
                 self._logged_in = True
             except:
                 raise ValueError("Response code is required for PKCE login!")
